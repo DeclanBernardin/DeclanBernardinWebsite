a8022d85d5bb609b3fcb9eba1e7cd9f9c23530e1
Declan Bernardin <dmjbernardin@yahoo.com>
2019-10-15 13:51:06 -0500

    Updates

---
+++ precache-manifest.84e3b417d591a789d9afe0f2f4f87d5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce39c1609df4a9bd062317af8b005c54",
+    "url": "/DeclanBernardinWebsite/index.html"
+  },
+  {
+    "revision": "03df251752ef5af70fbe",
+    "url": "/DeclanBernardinWebsite/static/css/main.9ae904f4.chunk.css"
+  },
+  {
+    "revision": "226f3c3c7d72e1b21b7d",
+    "url": "/DeclanBernardinWebsite/static/js/2.63d253df.chunk.js"
+  },
+  {
+    "revision": "03df251752ef5af70fbe",
+    "url": "/DeclanBernardinWebsite/static/js/main.75e3699e.chunk.js"
+  },
+  {
+    "revision": "e6f3f7a21bed61feedb0",
+    "url": "/DeclanBernardinWebsite/static/js/runtime-main.9f364173.js"
+  }
+]);
